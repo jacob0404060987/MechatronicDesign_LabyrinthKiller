48a514b684eb70e6db0d7b73ac659cd36fc379d7
Jacob0404060987 <kubapaw1256@gmail.com>
2023-10-28 13:40:34 +0200

    Robot jedzie, robot sie kreci, wszyscy tu sa pierdolnieci. Centrowanie chuja ssanie essa essa

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <hFramework.h>
+#include <hModules/DistanceSensor.h>
 
 static bool programRun=false;
 
+using namespace hModules;
 
-void encoder()
-{
-	while (true)
-	{
-		Serial.printf("pos: %d\r\n", hMot1.getEncoderCnt()); // print the current position of Motor 1 (no. of encoder ticks)
-		hLED1.toggle();
-		sys.delay(100);
-	}
-}
+DistanceSensor sens_front(hSens3);
+DistanceSensor sens_left(hSens4);
+DistanceSensor sens_right(hSens1);
 
-void onPress() // instruction executed by clicking the button 
+const int16_t set_distance=30;
+static int16_t act_distance=0;
+static int16_t diff_distance;
+static int16_t act_left_dist;
+static int16_t act_right_dist;
+static int16_t diff_side;
+
+void onPress() // instruction executed by clicking the buttson 
 {
 	if(programRun)
     {
@@ -26,14 +29,14 @@ void onPress() // instruction executed by clicking the button
 
 void motorStartFwd(void)
 {
-	motor_right.setPower(300);
-	motor_left.setPower(300);
+	motor_right.setPower(-300);
+	motor_left.setPower(-300);
 }
 
 void motorStartBwd(void)
 {
-	motor_right.setPower(-300);
-	motor_left.setPower(-300);
+	motor_right.setPower(300);
+	motor_left.setPower(300);
 }
 
 void motorStop(void)
@@ -42,36 +45,70 @@ void motorStop(void)
 	motor_left.setPower(0);
 }
 
-void motorTurnRight(void)
-{
-	motor_right.setPower(-300);
-	motor_left.setPower(300);
-}
-
 void motorTurnLeft(void)
 {
-	motor_right.setPower(300);
-	motor_left.setPower(-300);
+	motor_right.setPower(-250);
+	motor_left.setPower(250);
+}
+
+void motorTurnRight(void)
+{
+	motor_right.setPower(450);
+	motor_left.setPower(-450);
+}
+
+void motorCorrectionLeft(void)
+{
+	motor_right.setPower(-350);
+	motor_left.setPower(-150);
+}
+
+void motorCorrectionRight(void)
+{
+	motor_right.setPower(-150);
+	motor_left.setPower(-350);
+}
+void hInit(void)
+{
+
 }
 void hMain()
 {
-	sys.taskCreate(encoder); // this creates a task that will execute `encoder` concurrently
+	hInit();
 	while (true)
 	{
+		sys.delay(20);
         hBtn1.setOnPressHandler(onPress);
+		act_distance = sens_front.getDistance();
+		act_left_dist = sens_left.getDistance();
+		act_right_dist = sens_right.getDistance();
+		diff_distance = set_distance - act_distance;
+		diff_side = act_left_dist - act_right_dist; // ujemna wtedy kiedy blizej lewej
         if(programRun){
-		motorStartFwd();
-		sys.delay(2000);
-		motorStartBwd();
-		sys.delay(2000);
-		motorTurnLeft();
-		sys.delay(2000);
+		/*if(diff_distance>5)
+		{
+			motorStartBwd();
+		}else if(diff_distance<-5)
+		{
+			motorStartFwd();
+		}else{
 		motorTurnRight();
-		sys.delay(2000);
+		sys.delay(1000);*/
+		if(diff_side < -5)
+		{
+			motorCorrectionRight();
+		}else if (diff_side > 5)
+		{
+			motorCorrectionLeft();
+		}else
+		{
+			motorStartFwd();
+		}
         }else if(!programRun)
 		{
 			motorStop();
 		}
+		Serial.printf("Left: %5d, Right: %5d, Front: %5d \n",act_left_dist, act_right_dist, act_distance);
 	}
 }
 
